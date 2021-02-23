e64a70806e7af4d8caa59b82b6da704a47cde296
Cristian Wilson <ventures451@outlook.com>
2021-02-23 16:39:31 -0300

    autoload

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -4,9 +4,9 @@ namespace Hcode\DB;
 
 class Sql {
 
-	const HOSTNAME = "localhost";
+	const HOSTNAME = "127.0.0.1";
 	const USERNAME = "root";
-	const PASSWORD = "zaq1xsw2cde3";
+	const PASSWORD = "root";
 	const DBNAME = "db_ecommerce";
 
 	private $conn;
